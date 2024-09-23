c0590b2f642d27123cf0d75d7642f6c6fa6f7c3e
boweniac <boweniac@yeah.net>
2024-09-23 08:21:35 +0800

    增加角色知识库功能

--- plugins/wxagent/wxagent.py
+++ plugins/wxagent/wxagent.py
@@ -6,6 +6,7 @@ from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 from plugins import *
 from plugins.wxsop.chat_gpt_bot import OpenaiBot
+import requests
 
 
 @plugins.register(
@@ -40,8 +41,8 @@ class WXAgent(Plugin):
     def on_handle_context(self, e_context: EventContext):
         if e_context["context"].type != ContextType.TEXT:
             return
-
-        bot_wxid = e_context.econtext['channel'].user_id
+        bot_wxid = e_context.econtext['context'].kwargs['wxid']
+        logger.info("[WXAgent] bot_wxid={}".format(bot_wxid))
         contact_wxid = e_context.econtext['context'].kwargs['session_id']
         receiver = e_context.econtext['context'].kwargs['receiver']
         # nickname = e_context.econtext['context'].kwargs['msg'].from_user_nickname
@@ -52,24 +53,40 @@ class WXAgent(Plugin):
             contact_type = 2
 
         agent_info = db_storage.get_agent_info(bot_wxid)
+        logger.info("[WXAgent] agent_info={}".format(agent_info))
         # 调用 chatgpt 接口
         if agent_info:
-            prompt = f"""# 用户发送内容：{content}
+            # 优化问题
+            expand_system_prompt = f"""# 任务：
+请根据上下文信息，优化用户发送的最后一条消息，补齐消息中可能缺失的主语、谓语、宾语、定语、状语、补语句子成分。
 
-# 角色
+# 用户发送的最后一条消息：{content}
+
+# 回复要求
+1. 直接输出优化后的消息"""
+            expand_bot_reply = self.bot.reply_silent(e_context.econtext['context'], expand_system_prompt)
+            logger.debug("[wxagent] expand_bot_reply: %s" % expand_bot_reply.content)
+
+            answer = chat_service_openai_like(agent_info['dataset_id'], expand_bot_reply.content)
+
+            system_prompt = f"""# 角色
 {agent_info['role']}
 
 # 背景：
 {agent_info['background']}
-
-# 历史对话记录：
-{agent_info['examples']}
-
-# 回复要求：
+"""
+            if answer is not None:
+                system_prompt += """# 相关知识：
+"""
+                for data in answer:
+                    system_prompt += f"""问题：{data['q']}
+答案：{data['a']}
+"""
+            system_prompt += """# 回复要求：
 1. 直接以角色设定的角度回答问题，并以第一人称输出。
 2. 不要在回复前加角色、姓名。
 3. 回复要正式"""
-            bot_reply = self.bot.reply(prompt, e_context.econtext['context'])
+            bot_reply = self.bot.reply(content, e_context.econtext['context'], system_prompt)
             logger.debug("[wxagent] reply: %s" % bot_reply)
 
             reply = Reply()
@@ -81,8 +98,30 @@ class WXAgent(Plugin):
 
             e_context.action = EventAction.BREAK_PASS
 
-
     def get_help_text(self, **kwargs):
         help_text = "AI 角色 回复"
         return help_text
 
+
+def chat_service_openai_like(dataset_id: str, text: str):
+    headers = {"Content-Type": "application/json",
+               'Authorization': 'Bearer fastgpt-czbAiqKKse65hwwviZhwkgvyDEKE3aeB31Fx18oUsAGojyWQ672HdsWZi1E1C'}
+    args = {
+        "datasetId": dataset_id,
+        "text": text,
+        "limit": 100,
+        "similarity": 0.8,
+        "searchMode": "mixedRecall",
+        "usingReRank": False
+    }
+    response = requests.post("https://fastgpt.gkscrm.com/api/core/dataset/searchTest", headers=headers, json=args)
+    logger.debug("[wxsop] response: %s" % response)
+    logger.debug("[wxsop] response.status_code: %s" % response.status_code)
+    logger.debug("[wxsop] response.json(): %s" % response.json())
+    if response.status_code == 200:
+        response_json = response.json()
+        if "data" in response_json and "list" in response_json["data"]:
+            return response_json["data"]["list"]
+        else:
+            return None
+    return None
--- plugins/wxsop/chat_gpt_bot.py
+++ plugins/wxsop/chat_gpt_bot.py
@@ -35,31 +35,36 @@ class OpenaiBot(Bot, OpenAIImage):
         if conf().get("rate_limit_chatgpt"):
             self.tb4chatgpt = TokenBucket(conf().get("rate_limit_chatgpt", 20))
 
-        # self.sessions = SessionManager(ChatGPTSession, model=conf().get("model") or "gpt-3.5-turbo")
+        self.sessions = SessionManager(ChatGPTSession, model=conf().get("model") or "gpt-4o")
         self.args = {
             "model": model,  # 对话模型的名称
             "stream": False,  # 是否开启流模式
         }
 
-    def reply(self, query, context=None):
+    def reply(self, query, context=None, system_prompt: str = None):
         # acquire reply content
         if context.type == ContextType.TEXT:
             logger.info("[CHATGPT] query={}".format(query))
-
-            reply = None
-
+            session_id = "chatId-{}".format(context["wxid"] + "_" + format(context["session_id"]))
+            logger.info("[WXAgent] session_id={}".format(session_id))
+            session = self.sessions.session_query(query, session_id)
             new_args = self.args.copy()
-            new_args["messages"] = [{
-                "role": "user",
-                "content": query
-            }]
+            if system_prompt is not None:
+                new_args["messages"] = [
+                    {**message, 'content': system_prompt} if message['role'] == 'system' else message
+                    for message in session.messages
+                ]
+            else:
+                new_args["messages"] = session.messages
 
             reply_content = self.chat_service_openai_like(new_args)
-            reply = Reply(
-                ReplyType.TEXT,
-                reply_content,
-            )
-            return reply
+
+            if reply_content is not None:
+                self.sessions.session_reply(reply_content, session_id)
+                reply = Reply(ReplyType.TEXT, reply_content)
+                return reply
+            else:
+                return Reply(ReplyType.ERROR, "请让我思考一下哈，一会再问我吧")
 
         elif context.type == ContextType.IMAGE_CREATE:
             ok, retstring = self.create_img(query, 0)
@@ -69,6 +74,30 @@ class OpenaiBot(Bot, OpenAIImage):
             else:
                 reply = Reply(ReplyType.ERROR, retstring)
             return reply
+        else:
+            reply = Reply(ReplyType.ERROR, "Bot不支持处理{}类型的消息".format(context.type))
+            return reply
+    def reply_silent(self, context=None, system_prompt: str = None):
+        # acquire reply content
+        if context.type == ContextType.TEXT:
+            session_id = "chatId-{}".format(context["wxid"] + "_" + format(context["session_id"]))
+            session = self.sessions.build_session(session_id)
+            new_args = self.args.copy()
+            if system_prompt is not None:
+                new_args["messages"] = [
+                    {**message, 'content': system_prompt} if message['role'] == 'system' else message
+                    for message in session.messages
+                ]
+            else:
+                new_args["messages"] = session.messages
+
+            reply_content = self.chat_service_openai_like(new_args)
+
+            if reply_content is not None:
+                reply = Reply(ReplyType.TEXT, reply_content)
+                return reply
+            else:
+                return Reply(ReplyType.ERROR, "请让我思考一下哈，一会再问我吧")
         else:
             reply = Reply(ReplyType.ERROR, "Bot不支持处理{}类型的消息".format(context.type))
             return reply
