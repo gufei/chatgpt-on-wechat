a14614b924f40f489d7fc89a9676789980d79ac8
jimmyyem <jimmyyem@qq.com>
2025-03-26 20:15:23 +0800

    fix:回退代码

--- channel/wechat/workphone_channel.py
+++ channel/wechat/workphone_channel.py
@@ -84,7 +84,7 @@ class WorkPhoneChannel(ChatChannel):
 
     def startup(self):
         # 测试 credential: bwkf:rQRwCSOmplX3TtLJ
-        self.wsCli = WebSocketClient("ws://chat.gkscrm.com:13088", "bwkf:rQRwCSOmplX3TtLJ")
+        self.wsCli = WebSocketClient("ws://chat.gkscrm.com:13088", "")
         self.wsCli.start()
         self.wsCli.ws.on_message = self.on_message
 
